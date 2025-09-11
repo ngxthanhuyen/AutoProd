92fe4fddf9f3172690cc5c5a1463296f35799e2d
Uyen <146037408+ngxthanhuyen@users.noreply.github.com>
2025-09-11 21:58:50 +0700

    Update app.py

--- flask-application/app.py
+++ flask-application/app.py
@@ -44,7 +44,6 @@ def get_image(image_id):
         download_name=image_data['name']
     )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
 
